29775a54bcfb419feb14aa9e016ccc590aa4f53b
seil9998 <seil9998@gmail.com>
2025-10-21 14:27:05 +0900

    update image-ui

--- community/forms.py
+++ community/forms.py
@@ -5,6 +5,8 @@ from .models import Post, PostComment
 
 
 class PostForm(forms.ModelForm):
+    remove_image = forms.BooleanField(required=False, label="이미지 삭제")
+
     certificate = forms.ModelChoiceField(
         queryset=Certificate.objects.order_by("name"),
         required=True,
@@ -22,7 +24,7 @@ class PostForm(forms.ModelForm):
                 "rows": 8,
                 "placeholder": "내용을 입력해주세요.",
             }),
-            "image": forms.ClearableFileInput(attrs={
+            "image": forms.FileInput(attrs={
                 "accept": "image/*",
             }),
         }
@@ -39,6 +41,27 @@ class PostForm(forms.ModelForm):
         self.fields["title"].widget.attrs.setdefault("id", "post-title")
         self.fields["body"].widget.attrs.setdefault("id", "post-body")
         self.fields["image"].widget.attrs.setdefault("id", "post-image")
+        self.fields["remove_image"].widget.attrs.setdefault("id", "remove-image")
+        if not self.instance or not getattr(self.instance, "image", None):
+            self.fields["remove_image"].widget = forms.HiddenInput()
+        else:
+            self.fields["remove_image"].label = "현재 이미지 삭제"
+            self.fields["remove_image"].widget.attrs.setdefault("class", "image-upload__remove-checkbox")
+
+    def save(self, commit=True):
+        post = super().save(commit=False)
+        remove_image = self.cleaned_data.get("remove_image")
+        new_image = self.cleaned_data.get("image")
+
+        if remove_image and not new_image:
+            if post.pk and post.image:
+                post.image.delete(save=False)
+            post.image = None
+
+        if commit:
+            post.save()
+            self.save_m2m()
+        return post
 
 
 class PostCommentForm(forms.ModelForm):
